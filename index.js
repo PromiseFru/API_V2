0cad655c06c41612123bf8cd8d24667a78527235
Promise Fru <promisefru1@gmail.com>
2020-07-21 17:34:02 +0100

    content-type validation with express-validator

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
--- route/route.js
+++ route/route.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    // if(!req.is('application/json')){
-    //     res.sendStatus(400);
-    // }else{
-    //     res.send(req.body);
-    // }
-    res.send(req.headers["content-type"]);
+router.post('/', [
+    check('content-type')
+        .equals('application/json')
+        .withMessage('Has to be JSON only')
+    ] ,(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        res.send(req.body)
 })
 
 module.exports = router;
